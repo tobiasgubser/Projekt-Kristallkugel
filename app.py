6f6749fcde4bc8eb5747daece6dab1a168157e46
tobiasgubser <tobias.gubser.ost@gmail.com>
2026-05-03 20:09:28 +0200

    Add date import for dashboard functionality

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+from datetime import date
 
 st.set_page_config(
     page_title="SPI Case Study Dashboard",
