b95249521692244c67dd9743089f90a20fc8b36f
tobiasgubser <tobias.gubser.ost@gmail.com>
2026-05-04 20:54:27 +0200

    Reset index and filter news DataFrame columns

--- app.py
+++ app.py
@@ -148,6 +148,8 @@ if show_corr:
 
 st.subheader("Newsmeldungen des Tages")
 df_news_filtered = df_news[df_news.index.date == stichtag]
+df_news_filtered = df_news_filtered.reset_index(drop=True)
+df_news_filtered = df_news_filtered[["category", "text"]]
 st.dataframe(
     df_news_filtered,
     use_container_width=True
