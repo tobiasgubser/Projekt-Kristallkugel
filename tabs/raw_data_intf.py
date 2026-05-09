52377a3cfeea2ab1f9f255a26e05c18cc1d2cba8
tobiasgubser <tobias.gubser.ost@gmail.com>
2026-05-09 22:01:57 +0200

    Update raw_data_intf.py

--- tabs/raw_data_intf.py
+++ tabs/raw_data_intf.py
@@ -1,6 +1,15 @@
 # ---------------------------------------------------------
 # TAB – Raw Data
 # ---------------------------------------------------------
-with tab_raw:
-    st.header("📄 Raw Data")
-    st.dataframe(df_all.loc[df_all.index.date == stichtag].iloc[0], use_container_width=True)
+import streamlit as st
+
+def render_raw_data_tab(df_all, stichtag):
+    st.title("📄 Rohdaten")
+
+    df_stichtag = df_all.loc[df_all.index.date == stichtag]
+
+    if df_stichtag.empty:
+        st.warning("Keine Daten für diesen Stichtag.")
+        return
+
+    st.dataframe(df_stichtag.iloc[[0]], use_container_width=True)
