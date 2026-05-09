c4ac7aa81934e31e0a396301bb6645a11389baaf
tobiasgubser <tobias.gubser.ost@gmail.com>
2026-05-09 22:18:17 +0200

    Update raw_data_intf.py

--- tabs/raw_data_intf.py
+++ tabs/raw_data_intf.py
@@ -4,6 +4,9 @@
 import streamlit as st
 
 def render_raw_data_tab(df_all, stichtag):
+    st.write("Index sample:", df_all.index[:5])
+    st.write("Stichtag:", stichtag, type(stichtag))
+
     st.title("📄 Rohdaten")
 
     df_stichtag = df_all.loc[df_all.index.date == stichtag]
