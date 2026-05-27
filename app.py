89c84c220745fba2896ca4a34ceeefe52d62eb6d
Nextom <55537747+nextom1@users.noreply.github.com>
2026-05-27 20:17:37 +0200

    Add joblib import to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import joblib
 import plotly.express as px
 from app_utils import (
     normalize, compute_peer_deltas, compute_performance, 
