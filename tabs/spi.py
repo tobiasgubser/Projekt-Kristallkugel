751d9ced72e2bc17fdb64ff57bca304f469b3c2f
tobiasgubser <tobias.gubser.ost@gmail.com>
2026-05-28 23:55:50 +0200

    Update spi.py

--- tabs/spi.py
+++ tabs/spi.py
@@ -1,24 +1,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from app_utils import weather_icon, kpi_normal, kpi_color
+from app_utils import kpi_normal, kpi_color
 
 def render_spi_tab(df_all, stichtag, selected_cols, norm, deltas, compute_performance, handelstage):
-
-    # --- Wetterdaten ---
-    temp = df_all.loc[df_all.index.date == stichtag, "meteo_Temperatur (°C)"].iloc[0]
-    rain = df_all.loc[df_all.index.date == stichtag, "meteo_Niederschlagsdauer (min)"].iloc[0]
-    radiation = df_all.loc[df_all.index.date == stichtag, "meteo_Globalstrahlung (W/m²)"].iloc[0]
-    wind = df_all.loc[df_all.index.date == stichtag, "meteo_Windgeschwindigkeit (km/h)"].iloc[0]
-    humidity = df_all.loc[df_all.index.date == stichtag, "meteo_Relative Luftfeuchtigkeit (%)"].iloc[0]
-    icon = weather_icon(temp, rain, radiation, wind)
-
-    st.markdown(f"### Wetter des Tages")
-    c1, c2, c3, c4 = st.columns(4)
-    c1.metric("Temperatur", f"{icon} {temp: .1f} °C")
-    c2.metric("Niederschlagsdauer", f"{rain: .1f} min")
-    c3.metric("Wind", f"{wind: .1f} km/h")
-    c4.metric("Luftfeuchtigkeit", f"{humidity: .1f} %")
 
     # --- Performance je Variabel ---
     st.subheader("Performance bis Stichtag")
