225533f3351481dee7a4238f93724ca80f53d0fe
tobiasgubser <tobias.gubser.ost@gmail.com>
2026-05-28 23:34:37 +0200

    Update finance.py

--- tabs/finance.py
+++ tabs/finance.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from app.utils import kpi_normal, kpi_color
 
 def render_finance_tab(df_all, stichtag, norm, deltas, compute_performance, handelstage):
 
@@ -10,25 +11,12 @@ def render_finance_tab(df_all, stichtag, norm, deltas, compute_performance, hand
         perf_ytd, perf_week, perf_day = compute_performance(col, stichtag, df_all, handelstage)
         nominal = df_all.loc[df_all.index.date == stichtag, col].iloc[0]
 
-        def metric_block(label, value):
-            delta = f"{value:.2f}%"
-            delta_color = "normal"
-            if value > 0:
-                delta_color = "normal"
-            elif value < 0:
-                delta_color = "inverse"
-            return delta, delta_color
-
-        delta_ytd, color_ytd = metric_block("YTD", perf_ytd)
-        delta_week, color_week = metric_block("1 Woche", perf_week)
-        delta_day, color_day = metric_block("1 Tag", perf_day)
-
         def strip_prefix(name):
             return name.split("_", 1)[1] if "_" in name else name
 
         st.markdown(f"### {strip_prefix(col)}")
-        c1, c2, c3, c4 = st.columns(4)
-        c1.metric("Stand", f"{nominal:,.2f}")
-        c2.metric("YTD", "", delta_ytd, delta_color=color_ytd)
-        c3.metric("1 Woche", "", delta_week, delta_color=color_week)
-        c4.metric("1 Tag", "", delta_day, delta_color=color_day)
+        c1, c2, c3, c4 = st.columns(4)        
+        c1.markdown(kpi_normal("Stand", nominal), unsafe_allow_html=True)
+        c2.markdown(kpi_color("YTD (%)", perf_ytd), unsafe_allow_html=True)
+        c3.markdown(kpi_color("1 Woche (%)", perf_week), unsafe_allow_html=True)
+        c4.markdown(kpi_color("1 Tag (%)", perf_day), unsafe_allow_html=True)
